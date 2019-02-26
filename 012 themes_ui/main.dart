36908b38719fba7177a22b2d1fbffb642ad6db25
Andi Ganteng <andisyafrianda@gmail.com>
2019-02-27 01:23:53 +0700

    themes ui flutter

---
+++ 012 themes_ui/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/foundation.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // Define the default Brightness and Colors
+        brightness: Brightness.dark,
+        primarySwatch: Colors.lightBlue,
+        accentColor: Colors.redAccent,
+
+        // Define the default Font Family
+        fontFamily: 'Montserrat',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind')),
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).accentColor,
+          child: Text('Text with a background color',
+              style: Theme.of(context).textTheme.title),
+        ),
+      ),
+      floatingActionButton: Theme(
+        // Find and Extend the parent theme using "copyWith". Please see the next
+        // section for more info on `Theme.of`.
+        data: Theme.of(context).copyWith(accentColor: Colors.yellow),
+        child: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
